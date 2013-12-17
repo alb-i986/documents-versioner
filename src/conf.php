77a7a9c3a0d410b7450bf635ac27dec2e5d1efd2
Alberto Scotto <scotto.alberto.86@gmail.com>
2013-12-17 01:23:33 +0100

    polishing conf.php

--- src/conf.php
+++ src/conf.php
@@ -2,23 +2,18 @@
 
 $SRC_ROOT = __DIR__;
 $PROJECT_ROOT = realpath( $SRC_ROOT . '/..' );
-
-$SRC_ROOT = realpath( $PROJECT_ROOT . '/src' );
 $LIBS_ROOT = realpath( $PROJECT_ROOT . '/libs' );
 $PUBLIC_ROOT = realpath( $PROJECT_ROOT . '/public' );
 
 
+## importing 3d parties libraries
+
+require_once($LIBS_ROOT .'/html2pdf/html2pdf.class.php');
+
 
 ## Domain constants
-
-#$REPOS_ROOT = '/var/local/git';
-#$REPO_DIR = $REPOS_ROOT . '/documents';
 
 $DOCUMENTS_DIR = realpath( $PROJECT_ROOT . '/documents' );
 
 $SVN_USERNAME = 'www-data';
 $SVN_PASSWORD = 'tinymce';
-
-require_once('/usr/share/php/fpdf.php');
-require_once($LIBS_ROOT . '/glip/glip.php');
-require_once($LIBS_ROOT .'/html2pdf/html2pdf.class.php');
