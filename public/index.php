85d587bfd99d82b3870a702137bdfd7754747bfb
Alberto Scotto <scotto.alberto.86@gmail.com>
2013-12-17 01:22:09 +0100

    HTML skeleton bootstrapped (index.php)

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
   index.php builds the HTML with everything but the content
-  i.e. deals with 'html', 'head' and 'body' tags
+  i.e. deals mainly with 'html' and 'head' tags
 */
 
   require('../src/conf.php');
@@ -14,31 +14,26 @@
   }
 
 ?>
+<!DOCTYPE html>
 <html>
 
-<head>
+  <head>
+    <title>Documents Versioner</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-<!-- <script src="//tinymce.cachefly.net/4.0/tinymce.min.js"></script> -->
-<script src="js/tinymce/tinymce.min.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 
-<script type="text/javascript">
-  tinymce.init({
-    selector: "textarea#document-content",
-    theme: "modern",
-    width: 700,
-    height: 300,
-    plugins: [
-         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-         "save table contextmenu directionality emoticons template paste textcolor"
-   ],
-   toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons", 
-  });
-</script>
 
-</head>
+  </head>
 
-<body>
+  <body>
 
 
 <?php
@@ -50,6 +45,10 @@
   }
 ?>
 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
 
-</body>
+  </body>
 </html>
